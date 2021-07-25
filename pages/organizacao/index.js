807f64c47e543f259fce4370d76495ee54270585
Erik Santana <erik_s_marques@hotmail.com>
2021-07-25 18:17:25 -0300

    new: index reformated

--- pages/organizacao/index.js
+++ pages/organizacao/index.js
@@ -14,24 +14,51 @@ export default function Organizacao() {
   const [isLoading, setLoading] = useState(false);
   const [isCadastrado, setCadastrado] = useState(false);
   const [isOng, setOng] = useState(true);
+  const [useUser, setUser] = useState([]);
+  const [useErro, setErro] = useState();
   const router = useRouter();
   const handleCnpjInput = async (e) => {
     var value = e.target.value.replace(/\D/g, "");
     if ((isOng && value.length === 11) || (!isOng && value.length === 14)) {
       setLoading(true);
-      const res = await fetch(`/api/middleware/${value}`);
+      const res = await fetch(
+        `https://ecoshared-api.herokuapp.com/collectors/document/${value}`
+      );
       const data = await res.json();
       setLoading(false);
-      if (data) {
+      if (!data.status) {
+        const senha = CryptoJS.AES.decrypt(
+          data.senha,
+          process.env.NEXT_PUBLIC_PASSWORD_CRYPTO
+        ).toString(CryptoJS.enc.Utf8);
+        data.senha = senha;
+        await useUser.push(data);
         await setCadastrado(true);
       } else {
         const cpfCnpj = CryptoJS.AES.encrypt(
-          e.target.value.replace(/\D/g, ""),
+          value,
           process.env.NEXT_PUBLIC_PASSWORD_CRYPTO
         ).toString();
         localStorage.setItem("documento", cpfCnpj);
         router.push("organizacao/cadastrar-organizacao");
       }
+    }
+  };
+  const handleClickLogin = async (e) => {
+    e.preventDefault();
+    const senha = document?.getElementById("senha")?.value;
+    if (useUser[0].senha === senha) {
+      sessionStorage.setItem(
+        "documento",
+        CryptoJS.AES.encrypt(
+          useUser[0].documento,
+          process.env.NEXT_PUBLIC_PASSWORD_CRYPTO
+        ).toString()
+      );
+      sessionStorage.setItem("perfil", "organização");
+      router.push(`organizacao/dashboard`);
+    } else {
+      setErro("Senha incorreta!");
     }
   };
   const Login = () => {
@@ -41,7 +68,12 @@ export default function Organizacao() {
         className="flex mb-28 flex-col gap-6 flex-1 justify-center items-center self-center justify-self-center"
       >
         {isCadastrado ? (
-          <h1 className="preto text-5xl my-8 font-extrabold">faça seu login</h1>
+          <>
+            <h1 className="preto text-5xl my-4 font-extrabold">
+              faça seu login
+            </h1>
+            <h2 claclassName="preto text-3xl my-2 font-bold">{useErro}</h2>
+          </>
         ) : (
           <h1 className="preto text-5xl my-8 font-extrabold">
             para começar, digite seu {isOng ? "CPF" : "CNPJ"}
@@ -97,6 +129,7 @@ export default function Organizacao() {
               id="submit_organizacao"
               type="submit"
               value="fazer login"
+              onClick={(e) => handleClickLogin(e)}
             />
           </>
         ) : (
