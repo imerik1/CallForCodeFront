210565621e42c9ec303b8f40d79a597edcf77234
Erik Santana <erik_s_marques@hotmail.com>
2021-07-24 21:37:23 -0300

    new: api to get lat and lng

---
+++ pages/api/map/[address].js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+  const { address } = req.query;
+  fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAPS_API_KEY}`,
+    { method: "GET" }
+  ).then((response) => {
+    response
+      .json()
+      .then((data) => {
+        if (data?.results.length === 0) {
+          return res.status(500).json("Erro Interno");
+        }
+        return res.status(200).json(data?.results[0]?.geometry?.location);
+      })
+      .then((err) => {
+        return res.status(409).json(err);
+      });
+  });
+}
