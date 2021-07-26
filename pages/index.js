09bb287e94c30969fbb7e9949a64146abfc4e965
Erik Santana <erik_s_marques@hotmail.com>
2021-07-26 14:53:53 -0300

    hotfix: new index

--- pages/index.js
+++ pages/index.js
@@ -1,105 +1,51 @@
-import { useEffect } from "react";
-
-import Image from "next/image";
 import { useRouter } from "next/router";
 
-import { Header } from "../components/Header";
+import HeaderComponent from "../components/HeaderComponent";
+import FooterComponent from "../components/index/FooterComponent";
+import {
+  Body,
+  Button,
+  Main,
+  Slogan,
+} from "../styles/styles";
 
-export default function Inicio() {
-  const powereds = ["ibm", "microsoft", "shawee", "fcamara"];
+const Buttons = () => {
   const router = useRouter();
-  const handleClick = (e, uri) => {
-    e.preventDefault();
-    router.push(`/${encodeURIComponent(uri)}`);
-  };
-  useEffect(() => {
-    if (sessionStorage?.getItem("dados") && sessionStorage?.getItem("perfil")) {
-      if (sessionStorage.getItem("perfil") === "organização") {
-        router.push("/organizacao/dashboard");
-      } else {
-        router.push("/usuario/dashboard");
-      }
-    } else {
-      sessionStorage?.removeItem("dados");
-      sessionStorage?.removeItem("perfil");
-    }
-  }, []);
-  const Buttons = () => {
-    return (
-      <>
-        <button
-          onClick={(e) => handleClick(e, "usuario")}
-          className="branco text-xl verde-background py-1 px-6 rounded-lg"
-        >
-          sou usuário
-        </button>
-        <button
-          onClick={(e) => handleClick(e, "organizacao")}
-          className="verde-text text-xl py-1 px-6"
-        >
-          sou organização
-        </button>
-      </>
-    );
-  };
-
-  const Main = () => {
-    return (
-      <section className="flex px-24 flex-1 items-center">
-        <div style={{ maxWidth: "426px" }}>
-          <span className="preto text-7xl font-extrabold">
-            o novo jeito de{" "}
-            {
-              <span className="verde-text text-7xl font-extrabold">
-                reciclar
-              </span>
-            }{" "}
-            seus itens
-          </span>
-        </div>
-      </section>
-    );
-  };
-
-  const Footer = () => {
-    return (
-      <footer
-        style={{ height: "fit-content" }}
-        className="min-w-full flex flex-col justify-center items-center py-14"
-      >
-        <p className="preto text-md bold">powered by</p>
-        <div className="flex gap-14 justify-center items-center my-2">
-          {powereds.map((powered, id) => {
-            return (
-              <div
-                key={id}
-                style={{
-                  width: "150px",
-                  maxHeight: "50px",
-                  height: "50px",
-                  position: "relative",
-                }}
-              >
-                <Image
-                  src={`/${powered}.png`}
-                  layout="fill"
-                  objectFit="contain"
-                  alt={powered}
-                  className="logo_powered"
-                />
-              </div>
-            );
-          })}
-        </div>
-      </footer>
-    );
-  };
-
   return (
-    <div className="background">
-      {Header(Buttons)}
-      {Main()}
-      {Footer()}
-    </div>
+    <>
+      <Button
+        onClick={(e) => router.push("/usuario/login")}
+        color="third"
+        bgColor="primary"
+      >
+        sou usuário
+      </Button>
+      <Button
+        onClick={(e) => router.push("/organizacao/login")}
+        color="primary"
+      >
+        sou organização
+      </Button>
+    </>
+  );
+};
+
+export default function Index() {
+  return (
+    <Body>
+      <HeaderComponent Extend={Buttons()} />
+      <Main>
+        <div style={{ maxWidth: "426px" }}>
+          <Slogan color="secondary" weight="bold">
+            o novo jeito de{" "}
+            <Slogan color="primary" weight="bolder">
+              reciclar{" "}
+            </Slogan>
+            seus itens
+          </Slogan>
+        </div>
+      </Main>
+      <FooterComponent />
+    </Body>
   );
 }
