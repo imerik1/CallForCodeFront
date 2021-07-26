860da048e8c56c6ab61fb066b80b7c6a7048bafd
Erik Santana <erik_s_marques@hotmail.com>
2021-07-26 14:54:08 -0300

    feat: styled components e material ui ssr

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 /* eslint-disable @next/next/no-title-in-document-head */
 import Document, {
   Head,
@@ -5,100 +7,144 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-class MyDocument extends Document {
+import { ServerStyleSheets } from "@material-ui/core/styles";
+
+import { theme } from "./_app";
+
+export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+    const styledComponentsSheet = new ServerStyleSheet();
+    const materialSheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
 
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            styledComponentsSheet.collectStyles(
+              materialSheets.collect(<App {...props} />)
+            ),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <React.Fragment>
+            {" "}
+            {initialProps.styles} {materialSheets.getStyleElement()}{" "}
+            {styledComponentsSheet.getStyleElement()}{" "}
+          </React.Fragment>
+        ),
+      };
+    } finally {
+      styledComponentsSheet.seal();
+    }
+  }
   render() {
     return (
       <Html lang="pt-BR">
         <Head>
-          <title>EcoShared</title>
+          <title> EcoShared </title>{" "}
           <link
             rel="apple-touch-icon"
             sizes="57x57"
             href="/apple-icon-57x57.png"
-          />
+          />{" "}
           <link
             rel="apple-touch-icon"
             sizes="60x60"
             href="/apple-icon-60x60.png"
-          />
+          />{" "}
           <link
             rel="apple-touch-icon"
             sizes="72x72"
             href="/apple-icon-72x72.png"
-          />
+          />{" "}
           <link
             rel="apple-touch-icon"
             sizes="76x76"
             href="/apple-icon-76x76.png"
-          />
+          />{" "}
           <link
             rel="apple-touch-icon"
             sizes="114x114"
             href="/apple-icon-114x114.png"
-          />
+          />{" "}
           <link
             rel="apple-touch-icon"
             sizes="120x120"
             href="/apple-icon-120x120.png"
-          />
+          />{" "}
           <link
             rel="apple-touch-icon"
             sizes="144x144"
             href="/apple-icon-144x144.png"
-          />
+          />{" "}
           <link
             rel="apple-touch-icon"
             sizes="152x152"
             href="/apple-icon-152x152.png"
-          />
+          />{" "}
           <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/apple-icon-180x180.png"
-          />
+          />{" "}
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
             href="/android-icon-192x192.png"
-          />
+          />{" "}
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
             href="/favicon-32x32.png"
-          />
+          />{" "}
           <link
             rel="icon"
             type="image/png"
             sizes="96x96"
             href="/favicon-96x96.png"
-          />
+          />{" "}
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
             href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-          <meta name="theme-color" content="#b5e245" />
-        </Head>
+          />{" "}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          />{" "}
+          <link rel="manifest" href="/manifest.json" />{" "}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />{" "}
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />{" "}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />{" "}
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />{" "}
+          <meta charSet="utf-8" />{" "}
+          <meta name="msapplication-TileColor" content="#ffffff" />{" "}
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />{" "}
+          <meta name="theme-color" content={theme.primary} />{" "}
+        </Head>{" "}
         <body>
-          <Main />
-          <NextScript />
-          <script></script>
-        </body>
+          {" "}
+          <Main /> <NextScript />{" "}
+        </body>{" "}
       </Html>
     );
   }
 }
-
-export default MyDocument;
