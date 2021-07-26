eaa93ca69bcfd12aa3fedd563925ca014dafdf8b
Erik Santana <erik_s_marques@hotmail.com>
2021-07-26 14:54:22 -0300

    feat: cookies to store data

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
-import "../styles/tailwind.css";
-import "../styles/styles.css";
+import { CookiesProvider } from "react-cookie";
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyle from "../styles/global";
+
+export const theme = {
+  primary: "#b5e245",
+  secondary: "#363636",
+  third: "#ffffff",
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <CookiesProvider>
+        <Component {...pageProps} />
+      </CookiesProvider>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
